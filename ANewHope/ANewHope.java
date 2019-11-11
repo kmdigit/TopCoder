ca6f1010dc4b10838a473a6c778ea1b71d88f414
kmdigit <fiesta77@naver.com>
2019-11-11 13:16:38 +0900

    Create ANewHope.java

---
+++ ANewHope/ANewHope.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class ANewHope {
+    public static int count(int[] fs, int[] ls, int d) {
+        if (Arrays.equals(fs, ls)) return 1;
+        int n = fs.length;
+        boolean same = true;
+        int[] as = new int[n], bs = new int[n];
+        for(int i = 0; i<n; i++) {
+            as[fs[i]-1] = i;
+            bs[ls[i]-1] = i;
+        }
+        int min = 0;
+        for(int i = 0; i<n; i++) {
+          min = Math.min(min, bs[i] - as[i]);
+        }
+        return (-min+(n-d-1))/(n-d)+1;
+    }
+}
